916ebb7a3bbab060517c3c49c7bafb18428e1969
Ana Júlia Leite <leite.anajulia25@gmail.com>
2026-04-13 23:05:32 -0300

    atualização
    enviando código

---
+++ Calculando Porcentagem/aprendendoC#/Livro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aprendendoC_
+{
+    class Livro
+    {
+        public string titulo { get; set; }
+        public string autor { get; set; }
+        public int anoDePublicacao { get; set; }
+        public decimal preco { get; set; }
+
+
+        public void mostrarDetalhes()
+        {
+            // Formatação do preço com 2 casas decimais
+            Console.WriteLine($"título: {titulo}");
+            Console.WriteLine($"autor: {autor}");
+            Console.WriteLine($"ano de publicação: {anoDePublicacao}");
+            Console.WriteLine($"preco: {preco.ToString("F2")}");
+        }
+
+        public void AplicarDesconto(decimal desconto)
+        {
+            preco -= preco * (desconto / 100); //aplica o desconto
+        }
+    }
+}
---
+++ Calculando Porcentagem/aprendendoC#/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aprendendoC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //criando objeto da classe livre
+            Livro livro1 = new Livro
+            {
+                titulo = "Prince: A Private View",
+                autor = "Afshin Shahidi",
+                anoDePublicacao = 2017,
+                preco = 500.00m
+            };
+
+            //chamando o método mostrarDetalhes
+            livro1.mostrarDetalhes();
+
+            //chamando o método aplicarDesconto para aplicar um desconto de 5%
+            livro1.AplicarDesconto(5);
+
+            //mostrando o detalhe do livro após o desconto
+            Console.WriteLine("\nApós aplicar o desconto");
+            livro1.mostrarDetalhes();
+
+        }
+    }
+}
